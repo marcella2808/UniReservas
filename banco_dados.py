70bde2960ca2c551d2a18425522e6dc53f32fbb3
Marcella Carneiro <marcella.carneiro@a.fecaf.com.br>
2024-04-24 21:55:26 -0300

    uptade banco_dados.py

--- banco_dados.py
+++ banco_dados.py
@@ -15,20 +15,20 @@ class BancoDeDados:
         self.conectar()  # conecta com o banco
         self.cursor.execute('''CREATE TABLE IF NOT EXISTS usuarios ( 
                                 id INTEGER PRIMARY KEY,
-                                username TEXT NOT NULL,
-                                password TEXT NOT NULL)''')  # executa o comando SQL de criar tabela
+                                email TEXT NOT NULL,
+                                senha TEXT NOT NULL)''')  # executa o comando SQL de criar tabela
         self.conn.commit()  # salva alterações
         self.desconectar()  # desconecta do banco
 
-    def adicionar_usuario(self, username, password):
+    def adicionar_usuario(self, email, senha):
         self.conectar()
-        self.cursor.execute('''INSERT INTO usuarios (username, password) VALUES (?, ?)''', (username, password))
+        self.cursor.execute('''INSERT INTO usuarios (email, senha) VALUES (?, ?)''', (email, senha))
         self.conn.commit()
         self.desconectar()
 
-    def validar_login(self, username, password):
+    def validar_login(self, email, senha):
         self.conectar()
-        self.cursor.execute('''SELECT * FROM usuarios WHERE username = ? AND password = ?''', (username, password))  # realiza uma consulta
+        self.cursor.execute('''SELECT * FROM usuarios WHERE email = ? AND senha = ?''', (email, senha))  # realiza uma consulta
         usuario = self.cursor.fetchone()  # retorna os dados de usuário. Caso não houver, retorna None
         self.desconectar()
         return usuario is not None  # retorna True se usuario existir ou Falso se não existir
